66d86485ca792b760f7598116e283d6056975151
Chad Wade Day, Jr. <cdayjr@users.noreply.github.com>
2023-12-25 08:54:09 -0800

    print activity in rename script

--- rename.zsh
+++ rename.zsh
@@ -16,11 +16,13 @@ local safe_rename() {
     if [ "$ITEM" = "." ]; then
       continue
     fi
+    echo "$ITEM"
     local DIRECTORY="$(dirname "$ITEM")"
     local FILTERED="$(basename "$ITEM" | uconv -x "::Latin; ::Latin-ASCII; ([^\x00-\x7F]) > ;" | sed 's/[\?\/'"'"'\"<]/_/g')"
     local NEW_LOCATION="$DIRECTORY/$FILTERED"
     if [[ "$ITEM" != "$NEW_LOCATION" ]]; then
       mv "$ITEM" "$NEW_LOCATION"
+      echo Moved "$ITEM" to "$NEW_LOCATION"
     fi
     if [ -d "$NEW_LOCATION" ]; then
       safe_rename "$NEW_LOCATION"
